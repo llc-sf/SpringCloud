9af25833c53aefd2832ab25dec2b280072472757
陈潞潞 <chenlulu@yiche.com>
2018-03-04 15:48:14 +0800

    基础工程修改测试类名字

--- common-project/src/test/java/com/jadyer/demo/ApplicationTests.java
+++ common-project/src/test/java/com/jadyer/demo/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yuqiyu;
+package com.jadyer.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class Chapter4ApplicationTests {
+@SpringBootTest(classes = BootStarp.class)
+public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
